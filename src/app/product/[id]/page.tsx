8462cc8257a87df857b897705551cdf640d96e9f
Markobanduka <banduka44@gmail.com>
2024-10-21 12:55:14 +0200

    installed sidebar

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import ImageSlider from "@/components/ImageSlider";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Skeleton } from "@/components/ui/skeleton";
-import { createCheckout } from "@/lib/shopify/checkout";
-import { fetchProductById, ProductResponse } from "@/lib/shopify/shopify";
-import { useParams, useRouter } from "next/navigation";
 
+import { createCheckout } from "@/lib/shopify/checkout";
+import ImageSlider from "@/components/ImageSlider";
+import { fetchProductById, ProductResponse } from "@/lib/shopify/shopify";
+
+import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const SingleProduct = () => {
@@ -73,9 +75,12 @@ const SingleProduct = () => {
 
       {/* Product Details */}
       <div className="flex flex-col items-start space-y-6 lg:w-1/2">
-        <Button onClick={() => router.push("/")} className="mb-4">
-          Back
-        </Button>
+        <div className="flex justify-between items-center w-full">
+          <Button onClick={() => router.push("/")} className="">
+            Back
+          </Button>
+          <Button>Cart</Button>
+        </div>
 
         <h1 className="text-3xl lg:text-4xl font-bold">
           {product.product.title}
@@ -85,17 +90,26 @@ const SingleProduct = () => {
         </div>
 
         <p className="text-lg text-gray-700">{product.product.description}</p>
+        <div className="w-full flex justify-between">
+          <Button
+            className="bg-primary lg:w-auto py-3 px-6 mr-8"
+            onClick={handleBuy}
+            disabled={
+              product.product.priceRange.minVariantPrice.amount === "0.0"
+            }
+          >
+            {product.product.priceRange.minVariantPrice.amount === "0.0"
+              ? "Out of stock"
+              : "Buy"}
+          </Button>
 
-        <Button
-          // variant="outline"
-          className="bg-primary w-full lg:w-auto py-3 px-6"
-          onClick={handleBuy}
-          disabled={product.product.priceRange.minVariantPrice.amount === "0.0"}
-        >
-          {product.product.priceRange.minVariantPrice.amount === "0.0"
-            ? "Out of stock"
-            : "Buy"}
-        </Button>
+          <div className="flex items-center space-x-2">
+            <Button>-</Button>
+            <Button>Add to cart</Button>
+            <Input type="number" className="w-1/6" />
+            <Button>+</Button>
+          </div>
+        </div>
       </div>
     </div>
   );
