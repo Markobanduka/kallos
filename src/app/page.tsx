d182cd44ec51128b14e8bff6fc059c07b49819de
Markobanduka <banduka44@gmail.com>
2024-10-27 00:32:10 +0200

    adjusting color with top div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const ProductsPage = () => {
   return (
     <div>
       <LandingPage />
-      <div className="flex p-16 " id="products-section">
+      <div className="flex p-16" id="products-section">
         <div className="flex-1 flex justify-end">
           <h1 className="text-primary text-4xl font-semibold">Products</h1>
         </div>
